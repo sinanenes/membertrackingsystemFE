97e4bdf259b79647a51eb4cded6072efde43a262
sinanenes <sinanenes86@gmail.com>
2021-12-16 01:56:38 +0300

    kasabankafinished

--- src/app/kasabankalar/add-edit-kasabanka/add-edit-kasabanka.component.ts
+++ src/app/kasabankalar/add-edit-kasabanka/add-edit-kasabanka.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-kasabanka',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditKasabankaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
 
+  @Input() kasabanka: any;
+  KasaBankaId: string = "";
+  KasaBankaAdi: string = "";
+  KasaBankaTurKod: string = "";
+  Aciklama: string = "";
+  
   ngOnInit(): void {
+    this.KasaBankaId = this.kasabanka.KasaBankaId;
+    this.KasaBankaAdi = this.kasabanka.KasaBankaAdi;
+    this.KasaBankaTurKod = this.kasabanka.KasaBankaTurKod;
+    this.Aciklama = this.kasabanka.Aciklama;
+  }
+
+  addKasaBanka() {
+    var val = {
+      KasaBankaId: this.KasaBankaId,
+      KasaBankaAdi: this.KasaBankaAdi,
+      KasaBankaTurKod: this.KasaBankaTurKod,
+      Aciklama: this.Aciklama
+    };
+
+    this.service.addKasaBanka(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateKasaBanka() {
+    var val = {
+      KasaBankaId: this.KasaBankaId,
+      KasaBankaAdi: this.KasaBankaAdi,
+      KasaBankaTurKod: this.KasaBankaTurKod,
+      Aciklama: this.Aciklama
+    };
+    this.service.updateKasaBanka(val).subscribe(res => {
+      alert(res.toString());
+    });
   }
 
 }
--- src/app/kasabankalar/show-kasabanka/show-kasabanka.component.ts
+++ src/app/kasabankalar/show-kasabanka/show-kasabanka.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-kasabanka',
@@ -7,9 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowKasabankaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+
+  KasaBankalarList: any = [];
+
+  ModalTitle: string = "";
+  ActivateAddEditKasaBankaComp: boolean = false;
+  kasabanka: any;
+
+  KasaBankaIdFilter: string = "";
+  KasaBankaAdiFilter: string = "";
+  KasaBankalarListWithoutFilter: any = [];
 
   ngOnInit(): void {
+    this.refreshKasaBankalarList();
+  }
+  addClick() {
+    this.kasabanka = {
+      KasaBankaId: 0,
+      KasaBankaAdi: "",
+      KasaBankaTurKod: 1,
+      Aciklama:""
+    }
+    this.ModalTitle = "Kasa Banka Ekle";
+    this.ActivateAddEditKasaBankaComp = true;
+
   }
 
+  editClick(item: any) {
+    this.kasabanka = item;
+    this.ModalTitle = "Kasa Banka Duzelt";
+    this.ActivateAddEditKasaBankaComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Emin misiniz?')) {
+      this.service.deleteKasaBanka(item.KasaBankaId).subscribe(data => {
+        alert(data.toString());
+        this.refreshKasaBankalarList();
+      });
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditKasaBankaComp = false;
+    this.refreshKasaBankalarList();
+  }
+
+
+  refreshKasaBankalarList() {
+    this.service.getKasaBankalarList().subscribe(data => {
+      this.KasaBankalarList = data;
+      this.KasaBankalarListWithoutFilter = data;
+    });
+  }
+
+  filterFn() {
+    var KasaBankaIdFilter = this.KasaBankaIdFilter;
+    var KasaBankaAdiFilter = this.KasaBankaAdiFilter;
+    this.KasaBankalarList = this.KasaBankalarListWithoutFilter.filter(function (el: any) {
+      return el.KasaBankaId.toString().toLowerCase().includes(KasaBankaIdFilter.toString().trim().toLowerCase()) &&
+        el.KasaBankaAdi.toString().toLowerCase().includes(KasaBankaAdiFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.KasaBankalarList = this.KasaBankalarListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
+  }
 }
