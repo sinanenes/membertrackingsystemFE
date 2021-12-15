46a57c0126139bd62a086b707a1932434ba07d79
sinanenes <sinanenes86@gmail.com>
2021-12-16 02:18:50 +0300

    kodlarpagefinished

--- src/app/kodlar/add-edit-kod/add-edit-kod.component.ts
+++ src/app/kodlar/add-edit-kod/add-edit-kod.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-kod',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditKodComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service: SharedService) { }
+  
+  @Input() kod: any;
+  KodId: string = "";
+  KodAdi: string = "";
+  KodDeger: string = "";
+  KodGrup: string = "";
+  
   ngOnInit(): void {
+    this.KodId = this.kod.KodId;
+    this.KodAdi = this.kod.KodAdi;
+    this.KodDeger = this.kod.KodDeger;
+    this.KodGrup = this.kod.KodGrup;
+  }
+
+  addKod() {
+    var val = {
+      KodId: this.KodId,
+      KodAdi: this.KodAdi,
+      KodDeger: this.KodDeger,
+      KodGrup: this.KodGrup
+    };
+
+    this.service.addKod(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateKod() {
+    var val = {
+      KodId: this.KodId,
+      KodAdi: this.KodAdi,
+      KodDeger: this.KodDeger,
+      KodGrup: this.KodGrup
+    };
+    this.service.updateKod(val).subscribe(res => {
+      alert(res.toString());
+    });
   }
 
 }
--- src/app/kodlar/show-kod/show-kod.component.ts
+++ src/app/kodlar/show-kod/show-kod.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-kod',
@@ -7,9 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowKodComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+  
+  KodlarList: any = [];
 
+  ModalTitle: string = "";
+  ActivateAddEditKodComp: boolean = false;
+  kod: any;
+
+  KodIdFilter: string = "";
+  KodAdiFilter: string = "";
+  KodlarListWithoutFilter: any = [];
   ngOnInit(): void {
+    this.refreshKodlarList();
   }
 
+  addClick() {
+    this.kod = {
+      KodId: 0,
+      KodAdi: "",
+      KodDeger: "",
+      KodGrup: ""
+    }
+    this.ModalTitle = "Kod Ekle";
+    this.ActivateAddEditKodComp = true;
+
+  }
+
+  editClick(item: any) {
+    this.kod = item;
+    this.ModalTitle = "Kod Duzelt";
+    this.ActivateAddEditKodComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Emin misiniz?')) {
+      this.service.deleteKod(item.KodId).subscribe(data => {
+        alert(data.toString());
+        this.refreshKodlarList();
+      });
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditKodComp = false;
+    this.refreshKodlarList();
+  }
+
+
+  refreshKodlarList() {
+    this.service.getKodlarList().subscribe(data => {
+      this.KodlarList = data;
+      this.KodlarListWithoutFilter = data;
+    });
+  }
+
+  filterFn() {
+    var KodIdFilter = this.KodIdFilter;
+    var KodAdiFilter = this.KodAdiFilter;
+    this.KodlarList = this.KodlarListWithoutFilter.filter(function (el: any) {
+      return el.KodId.toString().toLowerCase().includes(KodIdFilter.toString().trim().toLowerCase()) &&
+        el.KodAdi.toString().toLowerCase().includes(KodAdiFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.KodlarList = this.KodlarListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
+  }
 }
