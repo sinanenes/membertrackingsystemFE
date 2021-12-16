138843892c968c6be6379bef0be901b3a02588fa
sinanenes <sinanenes86@gmail.com>
2021-12-17 00:20:09 +0300

    kurskayituyefinished

--- src/app/kurskayituyeler/add-edit-kurskayituye/add-edit-kurskayituye.component.ts
+++ src/app/kurskayituyeler/add-edit-kurskayituye/add-edit-kurskayituye.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-kurskayituye',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditKurskayituyeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service: SharedService) { }
+  
+  @Input() kurskayituye: any;
+  KursKayitUyeId: string = "";
+  KursId: string = "";
+  UyeId: string = "";
+  BaslangicTarih: string = "";
+  BitisTarih: string = "";
+  
   ngOnInit(): void {
+    this.KursKayitUyeId = this.kurskayituye.KursKayitUyeId;
+    this.KursId = this.kurskayituye.KursId;
+    this.UyeId = this.kurskayituye.UyeId;
+    this.BaslangicTarih = this.kurskayituye.BaslangicTarih;
+    this.BitisTarih = this.kurskayituye.BitisTarih;
+  }
+
+  addKursKayitUye() {
+    var val = {
+      KursKayitUyeId: this.KursKayitUyeId,
+      KursId: this.KursId,
+      UyeId: this.UyeId,
+      BaslangicTarih: this.BaslangicTarih,
+      BitisTarih: this.BitisTarih
+    };
+
+    this.service.addKursKayitUye(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateKursKayitUye() {
+    var val = {
+      KursKayitUyeId: this.KursKayitUyeId,
+      KursId: this.KursId,
+      UyeId: this.UyeId,
+      BaslangicTarih: this.BaslangicTarih,
+      BitisTarih: this.BitisTarih
+    };
+    this.service.updateKursKayitUye(val).subscribe(res => {
+      alert(res.toString());
+    });
   }
 
 }
--- src/app/kurskayituyeler/show-kurskayituye/show-kurskayituye.component.ts
+++ src/app/kurskayituyeler/show-kurskayituye/show-kurskayituye.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-kurskayituye',
@@ -7,9 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowKurskayituyeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+  KursKayitUyelerList: any = [];
+
+  ModalTitle: string = "";
+  ActivateAddEditKursKayitUyeComp: boolean = false;
+  kurskayituye: any;
+
+  KursKayitUyeIdFilter: string = "";
+  KursKayitUyelerListWithoutFilter: any = [];
+
 
   ngOnInit(): void {
+    this.refreshKursKayitUyelerList();
+  }
+
+  addClick() {
+    this.kurskayituye = {
+      KursKayitUyeId: 0,
+      KursId: 0,
+      UyeId: 0,
+      BaslangicTarih: "",
+      BitisTarih: ""
+    }
+    this.ModalTitle = "Kurs Kayıt Uye Ekle";
+    this.ActivateAddEditKursKayitUyeComp = true;
+
+  }
+
+  editClick(item: any) {
+    this.kurskayituye = item;
+    this.ModalTitle = "Kurs Kayıt Uye Duzelt";
+    this.ActivateAddEditKursKayitUyeComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Emin misiniz?')) {
+      this.service.deleteKursKayitUye(item.KursKayitUyeId).subscribe(data => {
+        alert(data.toString());
+        this.refreshKursKayitUyelerList();
+      });
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditKursKayitUyeComp = false;
+    this.refreshKursKayitUyelerList();
+  }
+
+
+  refreshKursKayitUyelerList() {
+    this.service.getKursKayitUyelerList().subscribe(data => {
+      this.KursKayitUyelerList = data;
+      this.KursKayitUyelerListWithoutFilter = data;
+    });
+  }
+
+  filterFn() {
+    var KursKayitUyeIdFilter = this.KursKayitUyeIdFilter;
+    this.KursKayitUyelerList = this.KursKayitUyelerListWithoutFilter.filter(function (el: any) {
+      return el.KursKayitUyeId.toString().toLowerCase().includes(KursKayitUyeIdFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.KursKayitUyelerList = this.KursKayitUyelerListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
   }
 
 }
