e43d37b6811f280c4dd85011cc1b3d8d5e55ff3e
sinanenes <sinanenes86@gmail.com>
2021-12-17 00:55:55 +0300

    odemelerFinished

--- src/app/odemeler/add-edit-odeme/add-edit-odeme.component.ts
+++ src/app/odemeler/add-edit-odeme/add-edit-odeme.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-odeme',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditOdemeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  @Input() odeme: any;
+  OdemeId: string = "";
+  KullaniciId: string = "";
+  KisiId: string = "";
+  OdemeTutar: string = "";
+  OdemeTarih: string = "";
+  OdemeTurKod: string = "";
+  DonemAyKod: string = "";
+  DonemYilKod: string = "";
+  KasaBankaId: string = "";
+  Aciklama: string = "";
 
   ngOnInit(): void {
+    this.loadOdemelerList();
   }
+  loadOdemelerList() {
+    this.OdemeId = this.odeme.OdemeId;
+    this.KullaniciId = this.odeme.KullaniciId;
+    this.KisiId = this.odeme.KisiId;
+    this.OdemeTutar = this.odeme.OdemeTutar;
+    this.OdemeTarih = this.odeme.OdemeTarih;
+    this.OdemeTurKod = this.odeme.OdemeTurKod;
+    this.DonemAyKod = this.odeme.DonemAyKod;
+    this.DonemYilKod = this.odeme.DonemYilKod;
+    this.KasaBankaId = this.odeme.KasaBankaId;
+    this.Aciklama = this.odeme.Aciklama;
+  }
+
+  addOdeme() {
+    var val = {
+      OdemeId: this.OdemeId,
+      KullaniciId: this.KullaniciId,
+      KisiId: this.KisiId,
+      OdemeTutar: this.OdemeTutar,
+      OdemeTarih: this.OdemeTarih,
+      OdemeTurKod: this.OdemeTurKod,
+      DonemAyKod: this.DonemAyKod,
+      DonemYilKod: this.DonemYilKod,
+      KasaBankaId: this.KasaBankaId,
+      Aciklama: this.Aciklama
+    };
+
+    this.service.addOdeme(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateOdeme() {
+    var val = {
+      OdemeId: this.OdemeId,
+      KisiId: this.KisiId,
+      OdemeTarih: this.OdemeTarih,
+      DonemAyKod: this.DonemAyKod,
+      DonemYilKod: this.DonemYilKod,
+      OdemeTurKod: this.OdemeTurKod,
+      OdemeTutar: this.OdemeTutar,
+      Aciklama: this.Aciklama
+    };
+    this.service.updateOdeme(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
 
 }
--- src/app/odemeler/show-odeme/show-odeme.component.ts
+++ src/app/odemeler/show-odeme/show-odeme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-odeme',
@@ -7,9 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowOdemeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  OdemelerList: any = [];
+
+  ModalTitle: string = "";
+  ActivateAddEditOdemeComp: boolean = false;
+  odeme: any;
+
+  OdemeIdFilter: string = "";
+  KisiIdFilter: string = "";
+  OdemelerListWithoutFilter: any = [];
 
   ngOnInit(): void {
+    this.refreshOdemelerList();
+  }
+
+  addClick() {
+    this.odeme = {
+      OdemeId: 0,
+      KullaniciId:0,
+      KisiId: 0,
+      OdemeTutar: 0,
+      OdemeTarih: "",
+      OdemeTurKod: "",
+      DonemAyKod: "",
+      DonemYilKod: "",
+      KasaBankaId: 0,
+      Aciklama: ""
+    }
+    this.ModalTitle = "Odeme Ekle";
+    this.ActivateAddEditOdemeComp = true;
+
+  }
+
+  editClick(item: any) {
+    this.odeme = item;
+    this.ModalTitle = "Odeme Duzelt";
+    this.ActivateAddEditOdemeComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Emin misiniz?')) {
+      this.service.deleteOdeme(item.OdemeId).subscribe(data => {
+        alert(data.toString());
+        this.refreshOdemelerList();
+      });
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditOdemeComp = false;
+    this.refreshOdemelerList();
+  }
+
+
+  refreshOdemelerList() {
+    this.service.getOdemelerList().subscribe(data => {
+      this.OdemelerList = data;
+      this.OdemelerListWithoutFilter = data;
+    });
+  }
+
+  filterFn() {
+    var OdemeIdFilter = this.OdemeIdFilter;
+    var KisiIdFilter = this.KisiIdFilter;
+    this.OdemelerList = this.OdemelerListWithoutFilter.filter(function (el: any) {
+      return el.OdemeId.toString().toLowerCase().includes(OdemeIdFilter.toString().trim().toLowerCase()) &&
+        el.KisiId.toString().toLowerCase().includes(KisiIdFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.OdemelerList = this.OdemelerListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
   }
 
 }
