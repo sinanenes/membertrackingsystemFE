dd8ab30a4a123eb49eacf6981d857c58cb6f09b1
sinanenes <sinanenes86@gmail.com>
2021-12-17 01:20:03 +0300

    sorularFinished

--- src/app/sorular/add-edit-soru/add-edit-soru.component.ts
+++ src/app/sorular/add-edit-soru/add-edit-soru.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-soru',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditSoruComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  @Input() soru: any;
+  SoruId: string = "";
+  KullaniciId: string = "";
+  SoruTarih: string = "";
+  CevapTarih: string = "";
+  SoruMetni: string = "";
+  CevapMetni: string = "";
+  CevaplayanId: string = "";
+  
 
   ngOnInit(): void {
+    this.loadSorularList();
   }
+  loadSorularList() {
+    this.SoruId = this.soru.SoruId;
+    this.KullaniciId = this.soru.KullaniciId;
+    this.SoruTarih = this.soru.SoruTarih;
+    this.CevapTarih = this.soru.CevapTarih;
+    this.SoruMetni = this.soru.SoruMetni;
+    this.CevapMetni = this.soru.CevapMetni;
+    this.CevaplayanId = this.soru.CevaplayanId;
+  }
+
+  addSoru() {
+    var val = {
+      SoruId: this.SoruId,
+      KullaniciId: this.KullaniciId,
+      SoruTarih: this.SoruTarih,
+      CevapTarih: this.CevapTarih,
+      SoruMetni: this.SoruMetni,
+      CevapMetni: this.CevapMetni,
+      CevaplayanId: this.CevaplayanId
+    };
+
+    this.service.addSoru(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateSoru() {
+    var val = {
+      SoruId: this.SoruId,
+      KullaniciId: this.KullaniciId,
+      SoruTarih: this.SoruTarih,
+      CevapTarih: this.CevapTarih,
+      SoruMetni: this.SoruMetni,
+      CevapMetni: this.CevapMetni,
+      CevaplayanId: this.CevaplayanId
+    };
+    this.service.updateSoru(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
 
 }
--- src/app/sorular/show-soru/show-soru.component.ts
+++ src/app/sorular/show-soru/show-soru.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-soru',
@@ -7,9 +8,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowSoruComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  SorularList: any = [];
+
+  ModalTitle: string = "";
+  ActivateAddEditSoruComp: boolean = false;
+  soru: any;
+
+  SoruIdFilter: string = "";
+  KullaniciIdFilter: string = "";
+  SorularListWithoutFilter: any = [];
 
   ngOnInit(): void {
+    this.refreshSorularList();
+  }
+
+  addClick() {
+    this.soru = {
+      SoruId: 0,
+      KullaniciId: 0,
+      SoruTarih: "",
+      CevapTarih: "",
+      SoruMetni: "",
+      CevapMetni: "",
+      CevaplayanId: 0
+    }
+    this.ModalTitle = "Soru Ekle";
+    this.ActivateAddEditSoruComp = true;
+
+  }
+
+  editClick(item: any) {
+    this.soru = item;
+    this.ModalTitle = "Soru Duzelt";
+    this.ActivateAddEditSoruComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Emin misiniz?')) {
+      this.service.deleteSoru(item.SoruId).subscribe(data => {
+        alert(data.toString());
+        this.refreshSorularList();
+      });
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditSoruComp = false;
+    this.refreshSorularList();
+  }
+
+
+  refreshSorularList() {
+    this.service.getSorularList().subscribe(data => {
+      this.SorularList = data;
+      this.SorularListWithoutFilter = data;
+    });
+  }
+
+  filterFn() {
+    var SoruIdFilter = this.SoruIdFilter;
+    var KullaniciIdFilter = this.KullaniciIdFilter;
+    this.SorularList = this.SorularListWithoutFilter.filter(function (el: any) {
+      return el.SoruId.toString().toLowerCase().includes(SoruIdFilter.toString().trim().toLowerCase()) &&
+        el.KullaniciId.toString().toLowerCase().includes(KullaniciIdFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.SorularList = this.SorularListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
   }
 
 }
