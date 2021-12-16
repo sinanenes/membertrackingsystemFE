7cba5c7712be8e3183c8c92ac2a858b11bd97aa0
sinanenes <sinanenes86@gmail.com>
2021-12-17 01:31:32 +0300

    tahsilatFinished

--- src/app/tahsilatlar/add-edit-tahsilat/add-edit-tahsilat.component.ts
+++ src/app/tahsilatlar/add-edit-tahsilat/add-edit-tahsilat.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-tahsilat',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditTahsilatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  @Input() tahsilat: any;
+  TahsilatId: string = "";
+  KisiId: string = "";
+  TahsilatTarih: string = "";
+  DonemAyKod: string = "";
+  DonemYilKod: string = "";
+  TahsilatTurKod: string = "";
+  TahsilatTutar: string = "";
+  KasaBankaId: string = "";
+  Aciklama: string = "";
 
   ngOnInit(): void {
+    this.loadTahsilatlarList();
   }
+  loadTahsilatlarList() {
+    this.TahsilatId = this.tahsilat.TahsilatId;
+    this.KisiId = this.tahsilat.KisiId;
+    this.TahsilatTarih = this.tahsilat.TahsilatTarih;
+    this.DonemAyKod = this.tahsilat.DonemAyKod;
+    this.DonemYilKod = this.tahsilat.DonemYilKod;
+    this.TahsilatTurKod = this.tahsilat.TahsilatTurKod;
+    this.TahsilatTutar = this.tahsilat.TahsilatTutar;
+    this.KasaBankaId = this.tahsilat.KasaBankaId;
+    this.Aciklama = this.tahsilat.Aciklama;
+  }
+
+  addTahsilat() {
+    var val = {
+      TahsilatId: this.TahsilatId,
+      KisiId: this.KisiId,
+      TahsilatTarih: this.TahsilatTarih,
+      DonemAyKod: this.DonemAyKod,
+      DonemYilKod: this.DonemYilKod,
+      TahsilatTurKod: this.TahsilatTurKod,
+      TahsilatTutar: this.TahsilatTutar,
+      KasaBankaId: this.KasaBankaId,
+      Aciklama: this.Aciklama
+    };
+
+    this.service.addTahsilat(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
+  updateTahsilat() {
+    var val = {
+      TahsilatId: this.TahsilatId,
+      KisiId: this.KisiId,
+      TahsilatTarih: this.TahsilatTarih,
+      DonemAyKod: this.DonemAyKod,
+      DonemYilKod: this.DonemYilKod,
+      TahsilatTurKod: this.TahsilatTurKod,
+      TahsilatTutar: this.TahsilatTutar,
+      KasaBankaId: this.KasaBankaId,
+      Aciklama: this.Aciklama
+    };
+    this.service.updateTahsilat(val).subscribe(res => {
+      alert(res.toString());
+    });
+  }
+
 
 }
--- src/app/tahsilatlar/show-tahsilat/show-tahsilat.component.ts
+++ src/app/tahsilatlar/show-tahsilat/show-tahsilat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-show-tahsilat',
@@ -7,9 +8,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowTahsilatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SharedService) { }
+
+  TahsilatlarList: any = [];
+
+  ModalTitle: string = "";
+  ActivateAddEditTahsilatComp: boolean = false;
+  tahsilat: any;
+
+  TahsilatIdFilter: string = "";
+  KisiIdFilter: string = "";
+  TahsilatlarListWithoutFilter: any = [];
 
   ngOnInit(): void {
+    this.refreshTahsilatlarList();
+  }
+
+  addClick() {
+    this.tahsilat = {
+      TahsilatId: 0,
+      KisiId: 0,
+      TahsilatTarih: "",
+      DonemAyKod: "",
+      DonemYilKod: "",
+      TahsilatTurKod: "",
+      TahsilatTutar: 0,
+      KasaBankaId: 0,
+      Aciklama: ""
+    }
+    this.ModalTitle = "Tahsilat Ekle";
+    this.ActivateAddEditTahsilatComp = true;
+
+  }
+
+  editClick(item: any) {
+    this.tahsilat = item;
+    this.ModalTitle = "Tahsilat Duzelt";
+    this.ActivateAddEditTahsilatComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Emin misiniz?')) {
+      this.service.deleteTahsilat(item.TahsilatId).subscribe(data => {
+        alert(data.toString());
+        this.refreshTahsilatlarList();
+      });
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditTahsilatComp = false;
+    this.refreshTahsilatlarList();
+  }
+
+
+  refreshTahsilatlarList() {
+    this.service.getTahsilatlarList().subscribe(data => {
+      this.TahsilatlarList = data;
+      this.TahsilatlarListWithoutFilter = data;
+    });
+  }
+
+  filterFn() {
+    var TahsilatIdFilter = this.TahsilatIdFilter;
+    var KisiIdFilter = this.KisiIdFilter;
+    this.TahsilatlarList = this.TahsilatlarListWithoutFilter.filter(function (el: any) {
+      return el.TahsilatId.toString().toLowerCase().includes(TahsilatIdFilter.toString().trim().toLowerCase()) &&
+        el.KisiId.toString().toLowerCase().includes(KisiIdFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any, asc: any) {
+    this.TahsilatlarList = this.TahsilatlarListWithoutFilter.sort(function (a: any, b: any) {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
   }
 
 }
